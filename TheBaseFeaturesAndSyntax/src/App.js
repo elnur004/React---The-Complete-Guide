c58c3f9746bf6af9b88616f7872c8698d7f7fc92
elnur004 <elnur@Macbook.local>
2020-05-17 01:24:36 +0400

    code fixed

--- TheBaseFeaturesAndSyntax/src/App.js
+++ TheBaseFeaturesAndSyntax/src/App.js
@@ -7,7 +7,7 @@ function App() {
   //    <h1>Hi, I'm a React App</h1>
   //   </div>
   // );
-  return React.createElement('div', null, 'h1', 'Hi, I\'m a React App'); 
+  return React.createElement('div', null, React.createElement('h1', null, 'Does this work now?')); 
   //return React.createElement(...) method gets compiled to JSX code behind the scenes. (even though we can't see that)
 }
 
