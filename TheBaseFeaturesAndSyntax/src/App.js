875f61415ecd13cc7cc248fb98336b82777758d6
elnur004 <elnur@Macbook.local>
2020-05-17 01:28:44 +0400

    back to JSX

--- TheBaseFeaturesAndSyntax/src/App.js
+++ TheBaseFeaturesAndSyntax/src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  // return (
-  //   <div className="App">
-  //    <h1>Hi, I'm a React App</h1>
-  //   </div>
-  // );
-  return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?')); 
+  return (
+    <div className="App">
+     <h1>Hi, I'm a React App</h1>
+    </div>
+  );
+  //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?')); 
   //return React.createElement(...) method gets compiled to JSX code behind the scenes. (even though we can't see that)
 }
 
