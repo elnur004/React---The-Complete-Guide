5546302cbfefa7d1ea1612dd9eec383a9f801b14
elnur004 <elnur@Macbook.local>
2020-05-17 01:21:09 +0400

    created element with React.createElement()

--- TheBaseFeaturesAndSyntax/src/App.js
+++ TheBaseFeaturesAndSyntax/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     <h1>Hi, I'm a React App</h1>
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //    <h1>Hi, I'm a React App</h1>
+  //   </div>
+  // );
+  return React.createElement('div', null, 'h1', 'Hi, I\'m a React App'); 
+  //return React.createElement(...) method gets compiled to JSX code behind the scenes. (even though we can't see that)
 }
 
 export default App;
