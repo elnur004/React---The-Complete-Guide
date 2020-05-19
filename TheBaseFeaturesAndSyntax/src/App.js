24e4c607a3b242030e0e7778183eac271dcc67a2
Elnur <47173552+elnur004@users.noreply.github.com>
2020-05-20 00:30:53 +0400

    added switchNameHandler method and onClick event

--- TheBaseFeaturesAndSyntax/src/App.js
+++ TheBaseFeaturesAndSyntax/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     <div className="App">
      <h1>Hi, I'm a React App</h1>
      <p>This is really working!</p>
-    <button onClick = {this.switchNameHandler}>Switch Name</button>
+    <button onClick={this.switchNameHandler}>Switch Name</button>
      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobbies: Racing</Person>
      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
