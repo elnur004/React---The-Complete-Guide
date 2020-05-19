26db49cffe48ceb23dd4bdcd555395ba83b1a310
Elnur <47173552+elnur004@users.noreply.github.com>
2020-05-19 18:43:32 +0400

    updated functional component to class based

--- TheBaseFeaturesAndSyntax/src/App.js
+++ TheBaseFeaturesAndSyntax/src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
+class App extends Component {  
+  render () {
   return (
     <div className="App">
      <h1>Hi, I'm a React App</h1>
@@ -15,6 +16,7 @@ function App() {
   );
   //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?')); 
   //return React.createElement(...) method gets compiled to JSX code behind the scenes. (even though we can't see that)
+  }
 }
 
 export default App;
