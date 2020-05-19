65689302cc5441adf9c1fbcf4fc1ea1583b3862a
Elnur <47173552+elnur004@users.noreply.github.com>
2020-05-19 19:08:57 +0400

    added state and changed dynamic propertys

--- TheBaseFeaturesAndSyntax/src/App.js
+++ TheBaseFeaturesAndSyntax/src/App.js
@@ -2,16 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {  
+class App extends Component {
+  state = {
+    persons: [
+      { name = 'John', age = 28 },
+      { name = 'Jane', age = 26 },
+      { name = 'Mike', age = 29 }
+    ]
+  }
   render () {
   return (
     <div className="App">
      <h1>Hi, I'm a React App</h1>
      <p>This is really working!</p>
     <button>Switch Name</button>
-     <Person name="John" age="28">My hobbies: Racing</Person>
-     <Person name="Jane" age="26"/>
-     <Person name="Mike" age="29"/>
+     <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobbies: Racing</Person>
+     <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+     <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
     </div>
   );
   //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?')); 
