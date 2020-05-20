2a7e10a56e07cfd526cfbe58b8fe0d2333e647f9
Elnur <47173552+elnur004@users.noreply.github.com>
2020-05-21 01:32:40 +0400

    using the useState() Hook for State Manipulation

--- TheBaseFeaturesAndSyntax/src/App.js
+++ TheBaseFeaturesAndSyntax/src/App.js
@@ -1,42 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+//class App extends Component {
+  const App = () => {
+    const [ personsState, setPersonsState ] = useState({
     persons: [
       { name = 'John', age = 28 },
       { name = 'Jane', age = 26 },
       { name = 'Mike', age = 29 }
     ],
     otherState: 'some other value'
-  }
-
-  switchNameHandler = () => {
+  })
+    
+    console.log(personsState);
+    
+    const switchNameHandler = () => {
     // DON'T DO THIS: this.state.persons[0].name = 'Maximillian';
-    setState({
+    setPersonsState({
       persons: [
       { name = 'Bob', age = 28 },
       { name = 'Jane', age = 26 },
       { name = 'Mike', age = 27 }
-    ]
+    ],
+      otherState: personsState.otherState
     })
   }
-
-  render () {
+    
   return (
     <div className="App">
      <h1>Hi, I'm a React App</h1>
      <p>This is really working!</p>
-    <button onClick={this.switchNameHandler}>Switch Name</button>
-     <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobbies: Racing</Person>
-     <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-     <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+    <button onClick={switchNameHandler}>Switch Name</button>
+     <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>My hobbies: Racing</Person>
+     <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+     <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
     </div>
   );
   //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?')); 
   //return React.createElement(...) method gets compiled to JSX code behind the scenes. (even though we can't see that)
   }
-}
+ 
+//}
 
 export default App;
