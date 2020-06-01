21d61b1c4cfaba6cdacc950197762388cf9eafd3
Elnur <47173552+elnur004@users.noreply.github.com>
2020-06-02 01:49:24 +0400

    created UserOutput component

---
+++ practicing--the-base-syntax/src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = () => {
+  return(
+    <div>
+      <p>Something in this paragraph<p>
+      <p>I hope i'll be overwritten!<p>
+    <div>
+  );
+};
+
+export default userOutput;
