0010b3e9aa5cf49f7c9d37e276007a837e31912e
Elnur <47173552+elnur004@users.noreply.github.com>
2020-06-02 02:06:14 +0400

    added user to UserOutput component

--- practicing--the-base-syntax/src/UserOutput/UserOutput.js
+++ practicing--the-base-syntax/src/UserOutput/UserOutput.js
@@ -3,7 +3,7 @@ import React from 'react';
 const userOutput = (props) => {
   return(
     <div>
-      <p>{props.userName} is in this paragraph<p>
+      <p>{props.user} is in this paragraph<p>
       <p>I hope i'll be overwritten!<p>
     <div>
   );
